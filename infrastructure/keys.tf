7123259ab1157333b43cca44ba9fefcef9428bb9
Ashok22555 <ashoknallam03@gmail.com>
2025-10-24 14:38:52 +0530

    Fix: Correct keys.tf SSH key pair syntax

--- infrastructure/keys.tf
+++ infrastructure/keys.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "k8s_key" {
   key_name   = "${var.cluster_name}-key"
-  public_key = var.ssh_public_key != "" ? var.ssh_public_key : "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC... (placeholder)"
+  public_key = var.ssh_public_key
 
   tags = {
     Name = "${var.cluster_name}-key"
